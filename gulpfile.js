ecb139bb02270c851da04aa9c3531fe7934d3940
Craig McNaughton <therealcraigmcn@gmail.com>
2021-08-15 17:56:18 -0700

    Implement HTML to Markdown
    - update layout
    - npm update
    - move to subdirectory for Netlify

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
+const { readFileSync } = require("fs");
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
 const hash = require("gulp-hash");
+const rewrite = require("gulp-rev-rewrite");
 const hashOptions = { template: "<%= name %>.<%= hash %><%= ext %>" };
 const hashFilename = "hash-manifest.json";
 const argv = require('minimist')(process.argv.slice(2))
 const env = argv.env ? argv.env : 'development'
 const output = {
-  development: './tmp',
-  production: './dist',
+  development: './tmp/markdown',
+  production: './dist/markdown',
 }
 const browserSync = require('browser-sync').create()
 
 // CSS
-gulp.task('css', (done) => {
+gulp.task('css', function () {
   const autoprefixer = require('gulp-autoprefixer');
   const cleancss = require('gulp-clean-css')
   const rename = require('gulp-rename')
 
-  gulp.src("./src/styles/index.css")
-      .pipe(autoprefixer())
-      .pipe(gulpif(env === "production", cleancss()))
-      .pipe(rename("./css/styles.css"))
-      .pipe(hash(hashOptions))
-      .pipe(gulp.dest(output[env]))
-      .pipe(
-          hash.manifest(hashFilename, {
-              deleteOld: true,
-              sourceDir: __dirname + output[env].substring(1),
-          })
-      )
-      .pipe(gulp.dest(output[env]));
-  done()
+  return gulp.src("./src/styles/index.css")
+    .pipe(autoprefixer())
+    .pipe(gulpif(env === "production", cleancss()))
+    .pipe(rename("./css/styles.css"))
+    .pipe(hash(hashOptions))
+    .pipe(gulp.dest(output[env]))
+    .pipe(
+      hash.manifest(hashFilename, {
+        deleteOld: true,
+        sourceDir: __dirname + output[env].substring(1),
+      })
+    )
+    .pipe(gulp.dest(output[env]));
 })
 
 // JS
@@ -41,50 +42,47 @@ const buffer = require('vinyl-buffer')
 const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
 
-gulp.task('js', (done) => {
+gulp.task('js', function () {
   const b = browserify({
     entries: 'src/scripts/index.js',
     debug: env === 'production',
   })
 
-  b.transform(
-      babelify.configure({
-          presets: ["@babel/preset-env"],
-          sourceMaps: env === "production",
-      })
+  return b.transform(
+    babelify.configure({
+      presets: ["@babel/preset-env"],
+      sourceMaps: env === "production",
+    })
   )
-      .bundle()
-      .pipe(source("js/scripts.js"))
-      .pipe(buffer())
-      .pipe(gulpif(env === "production", sourcemaps.init({ loadMaps: true })))
-      .pipe(gulpif(env === "production", uglify()))
-      .pipe(gulpif(env === "production", sourcemaps.write("./")))
-      .pipe(hash(hashOptions))
-      .pipe(gulp.dest(output[env]))
-      .pipe(
-          hash.manifest(hashFilename, {
-              deleteOld: true,
-              sourceDir: __dirname + output[env].substring(1),
-              append: true,
-          })
-      )
-      .pipe(gulp.dest(output[env]));
-  done()
+    .bundle()
+    .pipe(source("js/scripts.js"))
+    .pipe(buffer())
+    .pipe(gulpif(env === "production", sourcemaps.init({ loadMaps: true })))
+    .pipe(gulpif(env === "production", uglify()))
+    .pipe(gulpif(env === "production", sourcemaps.write("./")))
+    .pipe(hash(hashOptions))
+    .pipe(gulp.dest(output[env]))
+    .pipe(
+      hash.manifest(hashFilename, {
+        deleteOld: true,
+        sourceDir: __dirname + output[env].substring(1),
+        append: true,
+      })
+    )
+    .pipe(gulp.dest(output[env]));
 })
 
 // HTML
-gulp.task('html', (done) => {
-  const { readFileSync } = require("fs");
-  const rewrite = require("gulp-rev-rewrite");
+gulp.task('html', function () {
   const manifest = readFileSync(`${output[env]}/${hashFilename}`);
-  gulp.src("./src/**/*.html")
-      .pipe(rewrite({ manifest }))
-      .pipe(gulp.dest(output[env]));
-  done()
+
+  return gulp.src("./src/**/*.html")
+    .pipe(rewrite({ manifest }))
+    .pipe(gulp.dest(output[env]));
 })
 
 // Build
-gulp.task('build', gulp.parallel('css', 'js', 'html'))
+gulp.task('build', gulp.series('css', 'js', 'html'))
 
 // Reload browser
 gulp.task('reload', (done) => {
@@ -96,7 +94,8 @@ gulp.task('reload', (done) => {
 gulp.task('browserSync', () => {
   browserSync.init({
     port: 3003,
-    server: output[env],
+    server: './tmp',
+    startPath: `/markdown/index.html`,
     ui: false,
   })
   gulp.watch(
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,67 @@ const converter = new showdown.Converter({
     strikethrough: true,
     tables: true,
 });
-const editor = document.getElementById("editor");
-const content = document.getElementById("content");
-const source = document.getElementById("source");
-const formatted = document.getElementById("formatted");
-const html = document.getElementById("html");
 
-editor.querySelector("[contenteditable]").addEventListener("input", e => {
-    const convertedHtml = converter.makeHtml(e.target.innerText);
-    const clean = sanitize(convertedHtml, {
+const main = document.querySelector("main.grid");
+const preview = document.getElementById("preview");
+const previewHeader = preview.querySelector(".subheader")
+const previewContent = document.getElementById("preview-content");
+
+/* Editor */
+// https://ace.c9.io/#nav=howto
+const markdownEditor = ace.edit('markdown-editor', {
+    mode: 'ace/mode/markdown',
+    newLineMode: 'unix',
+    wrap: true,
+});
+const htmlEditor = ace.edit('html-editor', {
+    mode: 'ace/mode/html',
+    newLineMode: 'unix',
+    wrap: true,
+});
+
+markdownEditor.session.selection.on('changeSelection', e => {
+    markdownEditorChange();
+});
+markdownEditor.session.selection.on('changeCursor', e => {
+    markdownEditorChange();
+});
+
+const markdownEditorChange = () => {
+    if (document.activeElement.parentElement.id === "markdown-editor") {
+        const markdownValue = markdownEditor.getValue();
+        const convertedHtml = converter.makeHtml(markdownValue);
+
+        const clean = cleanHtml(convertedHtml);
+
+        previewContent.innerHTML = clean;
+
+        htmlEditor.setValue(clean);
+        htmlEditor.clearSelection();
+    }
+}
+
+htmlEditor.session.selection.on('changeSelection', e => {
+    htmlEditorChange();
+});
+htmlEditor.session.selection.on('changeCursor', e => {
+    htmlEditorChange();
+});
+
+const htmlEditorChange = () => {
+    if (document.activeElement.parentElement.id === "html-editor") {
+        const htmlValue = htmlEditor.getValue();
+        const convertedMarkdown = converter.makeMarkdown(htmlValue);
+
+        previewContent.innerHTML = cleanHtml(htmlValue);
+
+        markdownEditor.setValue(convertedMarkdown);
+        markdownEditor.clearSelection();
+    }
+}
+
+const cleanHtml = (html) => {
+    return sanitize(html, {
         allowedTags: sanitize.defaults.allowedTags.concat(["h1", "h2", "del"]),
         allowedAttributes: Object.assign(
             { "*": ["class", "style"] },
@@ -23,39 +75,56 @@ editor.querySelector("[contenteditable]").addEventListener("input", e => {
     })
         .trim()
         .replace(/&amp;nbsp;/g, "&nbsp;");
-    content.innerHTML = clean;
-    source.innerHTML = clean
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/\n/g, "<br>");
+}
 
-    if (formatted.style.height) {
-        formatted.style.height = ""; //reset height, then recalculate
-        formatted.style.height = editor.offsetHeight - html.offsetHeight + "px";
+/* Resize editor */
+// https://stackoverflow.com/questions/26233180/resize-a-div-on-border-drag-and-drop-without-adding-extra-markup/53220241
+let previewTop, touchDiff
+const HANDLE_SIZE = 4 // set in CSS
+
+/* Resize preview */
+const setPreviewHeight = e => {
+    if (e.type === "mousemove") previewTop = e.clientY
+
+    if (e.type === "touchmove") previewTop = e.touches[0].clientY - touchDiff
+
+    if (previewTop) {
+        const previewStyle = getComputedStyle(preview)
+        const maxHeight = parseInt(previewStyle.maxHeight)
+        const minHeight = parseInt(previewStyle.minHeight)
+        const numAreas = getComputedStyle(main).gridTemplateAreas.split("\" ").length
+
+        const newHeight = window.innerHeight - previewTop;
+        const setHeight = newHeight > maxHeight ? maxHeight : newHeight < minHeight ? minHeight : newHeight
+
+        if (setHeight !== newHeight) {
+            previewTop = window.innerHeight - setHeight
+        }
+
+        main.style.gridTemplateRows = `${numAreas === 3 ? "auto " : ""}auto ${setHeight}px`;
     }
-});
 
-document.getElementById('toggleSource').addEventListener("click", e => {
-    e.preventDefault();
+    markdownEditor.resize()
+    htmlEditor.resize()
+}
 
-    const source = document.getElementById('source');
-    const show = document.getElementById('source-show')
-    const hide = document.getElementById('source-hide')
-    const isHidden = source.hidden;
-
-    source.hidden = !isHidden
-    show.hidden = isHidden
-    hide.hidden = !isHidden
-
-    if (source.hidden) {
-        html.style.height =
-            html.querySelector("header").offsetHeight + "px";
-        formatted.style.height =
-            editor.offsetHeight - html.offsetHeight + "px";
-    } else {
-        html.style.height = "";
-        formatted.style.height = "";
+preview.querySelector(".subheader").addEventListener('mousedown', e => {
+    if (e.offsetY <= HANDLE_SIZE) { // only resize while handle is active
+        document.addEventListener('mousemove', setPreviewHeight, false);
     }
-})
+}, false);
+
+document.addEventListener('mouseup', () => {
+    document.removeEventListener('mousemove', setPreviewHeight, false);
+}, false);
+
+preview.querySelector(".subheader").addEventListener('touchstart', e => {
+    touchDiff = e.touches[0].clientY - previewHeader.getBoundingClientRect().top
+    document.addEventListener('touchmove', setPreviewHeight, false);
+}, false);
+
+document.addEventListener('touchend', () => {
+    document.removeEventListener('touchmove', setPreviewHeight, false);
+}, false);
+
+window.addEventListener('resize', setPreviewHeight, false);
