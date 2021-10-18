9c14789ce433cc4dd57cc35a1bba44d076792d0b
Craig McNaughton <therealcraigmcn@gmail.com>
2021-10-18 15:48:36 -0700

    Add Music Monday form

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,45 @@
-const { readFileSync } = require("fs");
+const {
+    readFileSync
+} = require("fs");
 const gulp = require('gulp')
 const gulpif = require('gulp-if')
 const hash = require("gulp-hash");
 const rewrite = require("gulp-rev-rewrite");
-const hashOptions = { template: "<%= name %>.<%= hash %><%= ext %>" };
+const hashOptions = {
+    template: "<%= name %>.<%= hash %><%= ext %>"
+};
 const hashFilename = "hash-manifest.json";
 const argv = require('minimist')(process.argv.slice(2))
 const env = argv.env ? argv.env : 'development'
 const output = {
-  development: './tmp',
-  production: './dist',
-  netlify: './netlify',
+    development: './tmp',
+    production: './dist',
+    netlify: './netlify',
 }
 const outputNetlify = `${output[env]}/markdown`
 const browserSync = require('browser-sync').create()
 
 // CSS
 gulp.task('css', function () {
-  const autoprefixer = require('gulp-autoprefixer');
-  const cleancss = require('gulp-clean-css')
-  const rename = require('gulp-rename')
+    const autoprefixer = require('gulp-autoprefixer');
+    const cleancss = require('gulp-clean-css')
+    const rename = require('gulp-rename')
 
-  return gulp.src("./src/styles/index.css")
-    .pipe(autoprefixer())
-    .pipe(gulpif(env !== "development", cleancss()))
-    .pipe(rename("./css/styles.css"))
-    .pipe(hash(hashOptions))
-    .pipe(gulp.dest(output[env]))
-    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
-    .pipe(
-      hash.manifest(hashFilename, {
-        deleteOld: true,
-        sourceDir: __dirname + output[env].substring(1),
-      })
-    )
-    .pipe(gulp.dest(output[env]))
-    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+    return gulp.src("./src/styles/index.css")
+        .pipe(autoprefixer())
+        .pipe(gulpif(env !== "development", cleancss()))
+        .pipe(rename("./css/styles.css"))
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+            })
+        )
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
 })
 
 // JS
@@ -47,67 +51,106 @@ const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
 
 gulp.task('js', function () {
-  const b = browserify({
-    entries: 'src/scripts/index.js',
-    debug: env !== "development",
-  })
-
-  return b.transform(
-    babelify.configure({
-      presets: ["@babel/preset-env"],
-      sourceMaps: env !== "development",
+    const b = browserify({
+        entries: 'src/scripts/index.js',
+        debug: env !== "development",
     })
-  )
-    .bundle()
-    .pipe(source("js/scripts.js"))
-    .pipe(buffer())
-    .pipe(gulpif(env !== "development", sourcemaps.init({ loadMaps: true })))
-    .pipe(gulpif(env !== "development", uglify()))
-    .pipe(gulpif(env !== "development", sourcemaps.write("./")))
-    .pipe(hash(hashOptions))
-    .pipe(gulp.dest(output[env]))
-    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
-    .pipe(
-      hash.manifest(hashFilename, {
-        deleteOld: true,
-        sourceDir: __dirname + output[env].substring(1),
-        append: true,
-      })
-    )
-    .pipe(gulp.dest(output[env]))
-    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+
+    return b.transform(
+            babelify.configure({
+                presets: ["@babel/preset-env"],
+                sourceMaps: env !== "development",
+            })
+        )
+        .bundle()
+        .pipe(source("js/scripts.js"))
+        .pipe(buffer())
+        .pipe(gulpif(env !== "development", sourcemaps.init({
+            loadMaps: true
+        })))
+        .pipe(gulpif(env !== "development", uglify()))
+        .pipe(gulpif(env !== "development", sourcemaps.write("./")))
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+                append: true,
+            })
+        )
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
 })
+
+gulp.task('mm-js', function () {
+    const b = browserify({
+        entries: 'src/scripts/music-monday.js',
+        debug: env !== "development",
+    })
+
+    return b.transform(
+            babelify.configure({
+                presets: ["@babel/preset-env"],
+                sourceMaps: env !== "development",
+            })
+        )
+        .bundle()
+        .pipe(source("js/mm.js"))
+        .pipe(buffer())
+        .pipe(gulpif(env !== "development", sourcemaps.init({
+            loadMaps: true
+        })))
+        .pipe(gulpif(env !== "development", uglify()))
+        .pipe(gulpif(env !== "development", sourcemaps.write("./")))
+        .pipe(hash(hashOptions))
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+        .pipe(
+            hash.manifest(hashFilename, {
+                deleteOld: true,
+                sourceDir: __dirname + output[env].substring(1),
+                append: true,
+            })
+        )
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+})
+
 
 // HTML
 gulp.task('html', function () {
-  const manifest = readFileSync(`${output[env]}/${hashFilename}`);
+    const manifest = readFileSync(`${output[env]}/${hashFilename}`);
 
-  return gulp.src("./src/**/*.html")
-    .pipe(rewrite({ manifest }))
-    .pipe(gulp.dest(output[env]))
-    .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
+    return gulp.src("./src/**/*.html")
+        .pipe(rewrite({
+            manifest
+        }))
+        .pipe(gulp.dest(output[env]))
+        .pipe(gulpif(env === 'netlify', gulp.dest(outputNetlify)))
 })
 
 // Build
-gulp.task('build', gulp.series('css', 'js', 'html'))
+gulp.task('build', gulp.series('css', 'js', 'mm-js', 'html'))
 
 // Reload browser
 gulp.task('reload', (done) => {
-  browserSync.reload()
-  done()
+    browserSync.reload()
+    done()
 })
 
 // Browser sync
 gulp.task('browserSync', () => {
-  browserSync.init({
-    port: 3003,
-    server: './tmp',
-    ui: false,
-  })
-  gulp.watch(
-    ['src/styles/**/*.css', 'src/scripts/**/*.js', 'src/**/*.html'],
-    gulp.series('build', 'reload')
-  )
+    browserSync.init({
+        port: 3003,
+        server: './tmp',
+        ui: false,
+    })
+    gulp.watch(
+        ['src/styles/**/*.css', 'src/scripts/**/*.js', 'src/**/*.html'],
+        gulp.series('build', 'reload')
+    )
 })
 
 // Dev server
---
+++ src/scripts/music-monday.js
@@ -0,0 +1,141 @@
+ready(() => {
+    const defaultYear = new Date().getFullYear()
+    const {
+        dayYmd,
+        dayYmd_Hs,
+        dayYmdT
+    } = nextMonday()
+    document.getElementById('date').value = dayYmd_Hs
+    template.postDateYmd = dayYmd
+    template.postDateAtom = dayYmdT
+    document.getElementById('coverYear').value = defaultYear
+    document.getElementById('originalYear').value = defaultYear
+    document.getElementById('markdown').innerHTML = template.text()
+})
+
+document.getElementById('generate').addEventListener('input', (e) => {
+    switch (e.target.id) {
+        case 'title':
+            template.title = e.target.value.trim()
+            template.titleFriendly = template.title
+                .toLowerCase()
+                .replace(/[^a-z0-9\-]/g, '-')
+                .replace('--', '-')
+            break
+        case 'date':
+            const postDate = new Date(e.target.value)
+            const postDateArray =
+                postDate !== 'Invalid Date' ?
+                postDate.toString().split(' ') :
+                Date.now.toString().split(' ')
+            template.postDateYmd = `${postDate.getFullYear()}-${(
+        postDate.getMonth() +
+        1 +
+        ''
+      ).padStart(2, 0)}-${(postDate.getDate() + '').padStart(2, 0)}`
+            template.postDateAtom = `${template.postDateYmd}T${
+        postDateArray[4]
+      }${postDateArray[5].substr(3)}` //2018-09-17T04:45:00-07:00
+            break
+        case 'originalVideo': //fallthrough
+        case 'coverVideo':
+            template[e.target.id + 'Id'] = e.target.value.split('/').reverse()[0]
+            break
+        default:
+            if (Object.keys(template).includes(e.target.id)) {
+                template[e.target.id] = e.target.value.trim()
+            }
+    }
+    document.getElementById('markdown').innerHTML = template.text()
+})
+
+function ready(fn) {
+    if (
+        document.attachEvent ?
+        document.readyState === 'complete' :
+        document.readyState !== 'loading'
+    ) {
+        fn()
+    } else {
+        document.addEventListener('DOMContentLoaded', fn)
+    }
+}
+
+const nextMonday = () => {
+    const now = new Date()
+    const x = now.getDay()
+    const days = x > 1 ? 8 - x : x === 1 ? 0 : 1
+    const day = new Date()
+    day.setDate(now.getDate() + days)
+    day.setHours(11)
+    day.setMinutes(45)
+    day.setSeconds(0)
+    const dayArray = day.toString().split(' ')
+    const dayYmd = `${day.getFullYear()}-${(day.getMonth() + 1 + '').padStart(
+    2,
+    0
+  )}-${(day.getDate() + '').padStart(2, 0)}`
+    const dayYmd_Hs = `${dayYmd} ${day.getHours()}:${day.getMinutes()}`
+    const dayYmdT = `${dayYmd}T${dayArray[4]}${dayArray[5].substr(3)}`
+    return {
+        dayYmd,
+        dayYmd_Hs,
+        dayYmdT,
+    }
+}
+
+const template = {
+    title: 'TITLE',
+    titleFriendly: 'title-friendly',
+    titleUrl: 'http://example.com',
+    postDateYmd: 'YYYY-MM-DD',
+    postDateAtom: 'YYYY-MM-DDTHH:MM:SSO', //e.g., 2018-09-17T04:45:00-07:00
+    originalYear: new Date().getFullYear(),
+    originalArtist: 'ORIGINAL ARTIST',
+    originalArtistUrl: 'http://example.com',
+    originalAlbum: 'ORIGINAL ALBUM',
+    originalAlbumUrl: 'http://example.com',
+    originalVideoId: 'ORIGINAL_ID',
+    coverYear: new Date().getFullYear(),
+    coverArtist: 'COVER ARTIST',
+    coverArtistUrl: 'http://example.com',
+    coverAlbum: 'COVER ALBUM',
+    coverAlbumUrl: 'http://example.com',
+    coverVideoId: 'COVER_ID',
+    complementaryText: '',
+    text() {
+        return `---
+${this.postDateYmd}-${this.titleFriendly}.md
+---
+title: "${this.title}"
+date: ${this.postDateAtom}
+excerpt: "This week’s instalment of Music Monday is ${this.title}. The ${this.originalYear} ${this.originalArtist} original and a ${this.coverYear} cover by ${this.coverArtist}."
+layout: post
+categories:
+- People
+- Music Monday
+tags:
+- covers
+- music
+- video
+- ${this.originalArtist}
+- ${this.coverArtist}
+---
+This week’s instalment of Music Monday is [_${this.title}_](${this.titleUrl}) by
+[${this.originalArtist}](${this.originalArtistUrl}) from their ${this.originalYear} album,
+[${this.originalAlbum}](${this.originalAlbumUrl}). The [${this.coverArtist}](${this.coverArtistUrl})
+version is from their ${this.coverYear} album, [${this.coverAlbum}](${this.coverAlbumUrl}).
+
+${this.complementaryText}
+
+&lt;div class="video-container"&gt;
+&lt;iframe width="560" height="315" src="https://www.youtube.com/embed/${this.originalVideoId}" frameborder="0" allowfullscreen title="Video: ${this.title} by ${this.originalArtist}"&gt;&lt;/iframe&gt;
+&lt;/div&gt;
+
+&lt;div class="video-container"&gt;
+&lt;iframe width="560" height="315" src="https://www.youtube.com/embed/${this.coverVideoId}" frameborder="0" allowfullscreen title="Video: ${this.title} by ${this.coverArtist}"&gt;&lt;/iframe&gt;
+&lt;/div&gt;
+---
+${this.title}: The ${this.originalYear} ${this.originalArtist} original and a ${this.coverYear} cover by ${this.coverArtist} #musicmonday https://craigmcn.ca/${this.titleFriendly}.html`
+    },
+}
